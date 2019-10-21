170fd9a43298d2d171baabc04cb024e6677452bd
danubiobwm <danubio.bwm@gmail.com>
2019-10-21 11:06:13 -0300

    Error no Teste Id, Verificação de e-mail

---
+++ __tests__/integration/user.test.js
@@ -0,0 +1,41 @@
+import request from 'supertest';
+
+import app from '../../src/app';
+
+import truncate from '../util/truncate';
+
+describe('User', () => {
+  beforeEach(async () => {
+    await truncate();
+  });
+
+  it('should be able to register', async () => {
+    const response = await request(app)
+      .post('/users')
+      .send({
+        name: 'Danubio de Araujo',
+        email: 'danubio@gmail.com',
+        password_hash: '123456',
+      });
+    expect(response.body).to.HaveProperty('id');
+  });
+
+  // Verificação de email duplicado
+  it('should not be able to register with duplicated email', async () => {
+    await request(app)
+      .post('/users')
+      .send({
+        name: 'Danubio de Araujo',
+        email: 'danubio@gmail.com',
+        password_hash: '123456',
+      });
+    const response = await request(app)
+      .post('/users')
+      .send({
+        name: 'Danubio de Araujo',
+        email: 'danubio@gmail.com',
+        password_hash: '123456',
+      });
+    expect(response.status).toBe(400);
+  });
+});
---
+++ src/routes.js
@@ -0,0 +1,12 @@
+import { Router } from 'express';
+
+import UserController from './app/controllers/UserController';
+
+const routes = new Router();
+
+// routes.post('/users', (req, res) => {
+//  return res.json({ id: 1 });
+// });
+routes.post('/users', UserController.store);
+
+export default routes;
